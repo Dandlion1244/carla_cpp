6c11a27550a6d186304da91ed73843accee7dade
Dandlion1244 <1244303526@qq.com>
2024-12-29 23:58:18 +0800

    Update ActorROS2Handler.h

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorROS2Handler.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorROS2Handler.h
@@ -11,15 +11,28 @@
 #include <compiler/enable-ue4-macros.h>
 
 /// 访问者类
+// 定义一个名为ActorROS2Handler的类，用于处理与ROS 2相关的Actor操作
 class ActorROS2Handler
 {
-    public:
-        ActorROS2Handler() = delete;
-        ActorROS2Handler(AActor *Actor, std::string RosName) : _Actor(Actor), _RosName(RosName) {};
+public:
+    // 构造函数是被删除的，这意味着不能默认构造ActorROS2Handler对象
+    ActorROS2Handler() = delete;
 
-        void operator()(carla::ros2::VehicleControl &Source);
+    // 带参数的构造函数，用于创建ActorROS2Handler对象
+    // 参数：
+    // AActor* Actor：指向Unreal Engine中Actor的指针
+    // std::string RosName：ROS 2节点的名称
+    ActorROS2Handler(AActor *Actor, std::string RosName) : _Actor(Actor), _RosName(RosName) {};
 
-    private:
-        AActor *_Actor {nullptr};
-        std::string _RosName;
+    // 重载函数调用运算符()，允许对象像函数一样被调用
+    // 参数：
+    // carla::ros2::VehicleControl& Source：一个引用，指向车辆控制信息的ROS 2消息
+    void operator()(carla::ros2::VehicleControl &Source);
+
+private:
+    // 成员变量，存储指向Unreal Engine中Actor的指针
+    AActor *_Actor {nullptr};
+
+    // 成员变量，存储ROS 2节点的名称
+    std::string _RosName;
 };
