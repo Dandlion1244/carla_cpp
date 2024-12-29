56dc4dc87da1a406320c9e8eb6fabd77c4c59c8d
Dandlion1244 <1244303526@qq.com>
2024-12-30 00:14:04 +0800

    Update TrafficGroupParser.cpp

--- LibCarla/source/carla/opendrive/parser/TrafficGroupParser.cpp
+++ LibCarla/source/carla/opendrive/parser/TrafficGroupParser.cpp
@@ -24,33 +24,46 @@ namespace parser {
     </userData>
   */
 
-  void TrafficGroupParser::Parse(
-      const pugi::xml_document & /*xml*/,
-      carla::road::MapBuilder & /* map_builder */) {
-    // pugi::xml_node opendrive_node = xml.child("OpenDRIVE");
-    // for (pugi::xml_node userdata_node = opendrive_node.child("userData");
-    //     userdata_node;
-    //     userdata_node = userdata_node.next_sibling("userData")) {
-    //   for (pugi::xml_node trafficgroup_node = userdata_node.child("trafficGroup");
-    //       trafficgroup_node;
-    //       trafficgroup_node = trafficgroup_node.next_sibling("trafficGroup")) {
-    //     uint16_t id = trafficgroup_node.attribute("id").as_int(0);
-    //     uint16_t redTime = trafficgroup_node.attribute("redTime").as_int(0);
-    //     uint16_t yellowTime = trafficgroup_node.attribute("yellowTime").as_int(0);
-    //     uint16_t greenTime = trafficgroup_node.attribute("greenTime").as_int(0);
-    //     log_debug("Found TrafficGroup with ID: ",
-    //         id,
-    //         "Times (",
-    //         redTime,
-    //         ", ",
-    //         yellowTime,
-    //         ", ",
-    //         greenTime,
-    //         ")");
-    //   }
-    //   map_builder.AddTrafficGroup(id, redTime, yellowTime, greenTime);
+ // TrafficGroupParser类中的Parse函数，用于解析OpenDRIVE XML文件中的交通组信息
+void TrafficGroupParser::Parse(
+      const pugi::xml_document & xml, // 传入的OpenDRIVE XML文档
+      carla::road::MapBuilder & map_builder) { // 传入的MapBuilder对象，用于构建道路地图
+
+  // 获取XML文档中名为"OpenDRIVE"的根节点
+  // pugi::xml_node opendrive_node = xml.child("OpenDRIVE");
+
+  // 遍历所有名为"userData"的子节点
+  // for (pugi::xml_node userdata_node = opendrive_node.child("userData");
+  //     userdata_node;
+  //     userdata_node = userdata_node.next_sibling("userData")) {
+
+    // 遍历所有名为"trafficGroup"的子节点
+    // for (pugi::xml_node trafficgroup_node = userdata_node.child("trafficGroup");
+    //     trafficgroup_node;
+    //     trafficgroup_node = trafficgroup_node.next_sibling("trafficGroup")) {
+
+      // 从当前"trafficGroup"节点中读取属性值
+      // uint16_t id = trafficgroup_node.attribute("id").as_int(0); // 交通组ID
+      // uint16_t redTime = trafficgroup_node.attribute("redTime").as_int(0); // 红灯时间
+      // uint16_t yellowTime = trafficgroup_node.attribute("yellowTime").as_int(0); // 黄灯时间
+      // uint16_t greenTime = trafficgroup_node.attribute("greenTime").as_int(0); // 绿灯时间
+
+      // 使用日志记录找到的交通组信息
+      // log_debug("Found TrafficGroup with ID: ",
+      //     id,
+      //     "Times (",
+      //     redTime,
+      //     ", ",
+      //     yellowTime,
+      //     ", ",
+      //     greenTime,
+      //     ")");
     // }
-  }
+
+    // 将解析到的交通组信息添加到MapBuilder中
+    // map_builder.AddTrafficGroup(id, redTime, yellowTime, greenTime);
+  // }
+}
 
 } // namespace parser
 } // namespace opendrive
